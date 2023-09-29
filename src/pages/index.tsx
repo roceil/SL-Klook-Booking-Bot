d3721fc551a6da4d2cc87fdf0af5e7e58b6ba54e
roceil <a0978006326@gmail.com>
2023-09-30 05:35:57 +0800

    完成初版可訂票機器人

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,160 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { FormEvent, useState, useEffect } from 'react'
+import axios from 'axios'
 
-const inter = Inter({ subsets: ['latin'] })
+interface ParsedData {
+  全票數量?: number
+  半票數量?: number
+  全名?: string
+  參加時間?: string
+  手機號碼?: string
+  單位?: string
+  訂單編號?: string // 新增訂單編號
+}
+
+interface ShowData {
+  bookingDate?: string
+  adultCount?: number
+  childCount?: number
+  orderNumber?: string
+  passengerName?: string
+  passengerPhone?: string
+}
 
 export default function Home() {
+  const [text, setText] = useState<string>('')
+  const [showData, setShowData] = useState<ShowData>({})
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const parseText = (inputText: string) => {
+    const lines = inputText.split('\n')
+    const foundData: ParsedData = {}
+    let orderNumber: string | undefined
+
+    // 使用正則表達式找出訂單編號
+    const orderNumberMatch = inputText.match(/訂單編號:\s*([^\n]+)/)
+    if (orderNumberMatch) {
+      orderNumber = orderNumberMatch[1].split(' ')[0]
+    }
+
+    foundData['訂單編號'] = orderNumber
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim()
+      const key = line.split(':')[0].trim()
+      if (['單位', '全名', '參加時間', '手機號碼'].includes(key)) {
+        const validKey = key as keyof ParsedData
+        foundData[validKey] = lines[i + 2].trim() as any
+      }
+    }
+
+    let fullTicketCount: number = 0
+    let halfTicketCount: number = 0
+    if (foundData.單位) {
+      const unitInfo = foundData.單位
+      const tickets = unitInfo.split(',')
+      for (const ticket of tickets) {
+        const fullTicketMatch =
+          ticket.match(/全票.*x\s*(\d+)/) || ticket.match(/往返船票.*x\s*(\d+)/)
+        const halfTicketMatch = ticket.match(/半票.*x\s*(\d+)/)
+        if (fullTicketMatch) {
+          fullTicketCount = parseInt(fullTicketMatch[1], 10)
+        }
+        if (halfTicketMatch) {
+          halfTicketCount = parseInt(halfTicketMatch[1], 10)
+        }
+      }
+    }
+    foundData['全票數量'] = fullTicketCount
+    foundData['半票數量'] = halfTicketCount
+    return foundData
+  }
+
+  const getOrderNumber = async (convertData: ParsedData) => {
+    // 組合全名和訂單編號
+    const combinedName = `${convertData['全名']} - ${convertData['訂單編號']}`
+
+    const res = await axios.post(
+      'https://tl-booking-bot-dev.zeabur.app/booking',
+      {
+        bookingDate: convertData['參加時間'],
+        adultCount: convertData['全票數量'],
+        childCount: convertData['半票數量'],
+        passengerName: combinedName, // 使用組合名稱
+        passengerPhone: convertData['手機號碼'],
+      },
+    )
+
+    setShowData(res.data)
+    setText('')
+    setLoading(false)
+  }
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    const convertData = parseText(text)
+    getOrderNumber(convertData)
+  }
+
+  useEffect(() => {
+    console.log('showData: ', showData)
+  }, [showData])
+
+  const renderObj = {
+    '出發日期：': showData.bookingDate,
+    '乘客姓名：': showData.passengerName,
+    '全票數量：': showData.adultCount,
+    '半票數量：': showData.childCount,
+    '手機號碼：': showData.passengerPhone,
+    '訂單編號：': showData.orderNumber,
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className='container flex justify-center items-center h-screen'>
+      <div>
+        <div
+          style={{ display: loading ? 'flex' : 'none' }}
+          className='w-screen h-screen bg-black/70 fixed top-0 left-0 z-10 flex justify-center items-center'
+        >
+          <span className='loading loading-ring loading-lg'></span>
+          <span className='loading loading-ring loading-lg'></span>
+          <span className='loading loading-ring loading-lg'></span>
+        </div>
+        <h1 className='text-5xl font-bold  rounded backdrop-blur-sm bg-white/30 p-3 text-gray-800 mb-3'>
+          Klook 訂票系統
+        </h1>
+
+        <div className='flex space-x-4'>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              className='w-full h-full min-h-[500px] min-w-[500px] rounded backdrop-blur-sm bg-white/30 p-2 text-gray-800'
+              value={text}
+              onChange={e => setText(e.target.value)}
             />
-          </a>
+            <button
+              type='submit'
+              className=' bg-gray-800 hover:bg-white hover:text-gray-800 text-white font-bold py-3 px-7 text-xl rounded'
+            >
+              送出
+            </button>
+          </form>
+
+          <div className='min-w-[250px] text-gray-800 px-4 pt-3 rounded backdrop-blur-sm bg-white/30'>
+            <div className='space-y-4'>
+              <h2 className='text-2xl font-bold mb-10'>訂票結果</h2>
+
+              {Object.keys(renderObj).map(key => (
+                <p
+                  key={key}
+                  className='border-b pb-2 border-gray-800'
+                >
+                  {key}
+                  <b>{renderObj[key as keyof typeof renderObj]}</b>
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
