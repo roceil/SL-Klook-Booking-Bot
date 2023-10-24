35b1715d3c06a3e80ab63cbca0f52648873e7bfb
roceil <a0978006326@gmail.com>
2023-10-24 17:21:29 +0800

    test(_test_/*.ts): 增加Unit Test

---
+++ src/lib/getOrderNumber.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { ShowData } from '../pages'
+import { showAlertWithTimer } from './showAlertWithTimer'
+
+export type ParsedData = {
+  convertData: {
+    departureDate: string
+    passengerName: string
+    adultCount: number
+    childCount: number
+    passengerPhone: string
+    orderNumber: string
+  }
+  setShowData: React.Dispatch<React.SetStateAction<ShowData>>
+  setText: React.Dispatch<React.SetStateAction<string>>
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+export const getOrderNumber = async ({
+  convertData,
+  setShowData,
+  setText,
+  setLoading,
+}: ParsedData) => {
+  try {
+    const combinedName = `${convertData.passengerName} - ${convertData.orderNumber}`
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/booking`, {
+      bookingDate: convertData.departureDate,
+      adultCount: convertData.adultCount,
+      childCount: convertData.childCount,
+      passengerName: combinedName,
+      passengerPhone: convertData.passengerPhone,
+    })
+    setShowData(res.data)
+    setText('')
+    setLoading(false)
+    showAlertWithTimer('success', '訂票成功')
+    return res.data; 
+  } catch (err) {
+    showAlertWithTimer('error', '訂單獲取失敗')
+    setLoading(false)
+  }
+}
